6ade1552b9bcb10824bd5388e78e9f12261f4fa1
Osman Onur KUZUCU <kuzucu48@gmail.com>
2022-05-24 21:44:31 +0300

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+
+public static void main(String[] args) {
+    /*HeapPriorityQueue new2=new HeapPriorityQueue();
+    new2.insert(2,"A");
+    new2.insert(3,"B");
+    new2.insert(4,"C");
+    new2.insert(10,"K");*/
+    LinkedBinaryTree<String> tree=new LinkedBinaryTree<>();
+tree.addRoot("C");
+
+}
+}
